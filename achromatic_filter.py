db7130327439f8e78dece172175277f7d58aa824
Xander <92168720+AmbivalentBrain@users.noreply.github.com>
2025-12-05 21:40:00 -0600

    tilt correction, morphology, minor edits

--- achromatic_filter.py
+++ achromatic_filter.py
@@ -34,3 +34,24 @@ out_img = cv.imread(out_img_name, cv.IMREAD_GRAYSCALE)
 ret3,th3 = cv.threshold(out_img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 
 cv.imwrite(f"{out_img_name.replace(".jpg", "")}_otsu_gauss.jpg",th3)
+
+def achro_filter(img):
+    # make it smaller to make computing faster (takes forever with 4k image)
+    col_img = cv.resize(img, (int(img.shape[1]/4), int(img.shape[0]/4)))
+    # reduce noise with blur
+    col_img = cv.GaussianBlur(col_img, (5,5), 0)
+    # create an empty results array
+    res = np.ndarray(col_img.shape)
+
+    # closeness in value, k
+    k = 20
+
+    # https://stackoverflow.com/questions/52278546/how-can-i-iterate-through-numpy-3d-array
+    # set pixels that are too colorful to white, retain pixel otherwise
+    # should erase the rest of the image basically
+    for ij in np.ndindex(col_img.shape[:2]):
+        # print(f"{col_img[ij][0]} - {col_img[ij][1]} = {int(col_img[ij][0]) - int(col_img[ij][1])}")
+        res[ij] = col_img[ij] if ((np.abs(int(col_img[ij][0]) - int(col_img[ij][1])) <= k) &
+            (np.abs(int(col_img[ij][2]) - int(col_img[ij][1])) <= k) &
+            (np.abs(int(col_img[ij][0]) - int(col_img[ij][2])) <= k)) else [255,255,255]
+    return res
--- qr_filtering_x.py
+++ qr_filtering_x.py
@@ -30,8 +30,13 @@ ret3,otsu = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 # cv.imwrite(f"{out_path}_otsu_gauss.jpg",otsu)
 
 adp_gauss = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,int(2*np.round(img.shape[0]/80)+3),2) # 83 = height/50->rounded to odd+2?
+            cv.THRESH_BINARY,int(2*np.round(img.shape[0]/80)+3),2) # 83 = height/50->rounded to odd+2? for complex image
 cv.imwrite(f"{out_path}_adp_gauss.jpg",adp_gauss)
+
+kernel = np.ones((3,3),np.uint8)
+morphology_ver = cv.morphologyEx(adp_gauss, cv.MORPH_CLOSE, kernel)
+morphology_ver = cv.morphologyEx(morphology_ver, cv.MORPH_OPEN, kernel)
+cv.imwrite(f"{out_path}_morphology.jpg",morphology_ver)
 
 # bilateral = cv.bilateralFilter(img, 7, 75, 75)
 # cv.imwrite(f"{out_path}_bilateral.jpg", bilateral)
